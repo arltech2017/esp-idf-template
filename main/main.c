f13d1f5ec620d7b9ba47ea28393f57241872293b
Marco Sirabella <marco@sirabella.org>
2017-11-09 12:07:20 -0500

    Change service and characteristic ID to be unique

--- main/main.c
+++ main/main.c
@@ -50,8 +50,8 @@
 ///Declare the static function
 static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
 
-#define GATTS_SERVICE_UUID_TEST_A   0x00FF
-#define GATTS_CHAR_UUID_TEST_A      0xFF01
+#define GATTS_SERVICE_UUID_TEST_A   0xC2BD
+#define GATTS_CHAR_UUID_TEST_A      0xC24C
 #define GATTS_DESCR_UUID_TEST_A     0x3333
 #define GATTS_NUM_HANDLE_TEST_A     4
 
